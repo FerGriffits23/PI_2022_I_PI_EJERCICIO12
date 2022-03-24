cc140c467327e8ac3dfc38ac5553bce78d654800
unknown <Fernando.Griffits8@ujcv.edu.hn>
2022-03-24 11:44:59 -0600

    Ejercicio12

---
+++ PI_2022_I_PI_EJERCICIO12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_2022_I_PI_EJERCICIO12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ciclos inciertos
+
+            int numero;
+            Console.WriteLine("Escriba un numero");
+            numero = int.Parse(Console.ReadLine());
+
+            while ((numero >20) || (numero<20))
+            {
+                Console.WriteLine("ingrese un numero");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
